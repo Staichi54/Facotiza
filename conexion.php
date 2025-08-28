f5042eddc133bb69435803e3852b7860419e930a
Staichi54 <73539099+Staichi54@users.noreply.github.com>
2025-08-28 13:15:35 -0500

    Update conexion.php 2

--- conexion.php
+++ conexion.php
@@ -1,5 +1,5 @@
 <?php
-$serverName = "db26219.public.databaseasp.ne";
+$serverName = "db26219.public.databaseasp.net,1433"; // 👈 corregido
 $database   = "db26219";
 $uid        = "db26219";
 $pwd        = "SANTAFE2025!";
@@ -8,16 +8,16 @@ $connectionInfo = array(
     "Database" => $database,
     "UID" => $uid,
     "PWD" => $pwd,
+    "Encrypt" => true,               // 👈 requerido
+    "TrustServerCertificate" => true, // 👈 requerido
     "CharacterSet" => "UTF-8"
 );
 
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 
 if ($conn === false) {
-    die("❌ Error de conexión: <br>" . print_r(sqlsrv_errors(), true));
+    die("❌ Error de conexión:<br>" . print_r(sqlsrv_errors(), true));
 } else {
-    // echo "Conexión exitosa a SQL Server";
+    echo "✅ Conexión exitosa a SQL Server";
 }
-
 ?>
-
